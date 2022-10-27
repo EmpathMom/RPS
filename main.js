cf2fa8148900ebc420b1e379c667752bb2228ac2
EmpathMom <gaylenedavison87@gmail.com>
2022-10-26 19:08:15 -0700

    rpc final

--- main.js
+++ main.js
@@ -1,38 +1,77 @@
 const choices = ["rock","paper","scissors"]
-
-function game(){
- playRound();
+const winners = [];
+function game() {
+   for (let i = 1; i <= 5; i++) {
+    playRound(i);
+   }
+   document.querySelector('button').textContent= 'Play again';
+   logWins();
 }
-function playRound(){
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
+function playRound(round) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection,computerSelection);
+    winners.push(winner);
+    logRound(playerSelection,computerSelection,winner,round);
 }
 
 function playerChoice () {
-   let input = prompt('Type Rock, Paper, or Scissors');
+   let input = prompt("Type Rock, Paper, or Scissors");
    while (input ==null) {
-    input = prompt('Type Rock, Paper, or Scissors');
+    input = prompt("Type Rock, Paper, or Scissors");
    }
    input = input.toLowerCase();
    let check = validateInput(input);
-   if(check == true){
-    console.log(input);
+   while (check == false) {
+   input = prompt(
+    "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitalization doesn't matter"
+    );
+     while (input ==null) {
+    input = prompt("Type Rock, Paper, or Scissors");
    }
+    input = input.toLowerCase();
+    check = validateInput(input);
+    }
 
-   //console.log(input);
+  return input;
 }
 
-function computerChoice () {
+function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function validateInput(choice) {
-    if(choices.includes(choice)) {
-        return true;
-    } else {
-        return false;
-    }
+    return choices.includes(choice);
 }
 
+function checkWinner(choiceP, choiceC) {
+   if(choiceP === choiceC) {
+    return "Tie";
+   } else if(
+    (choiceP ==="rock" && choiceC =="scissors") ||
+    (choiceP ==="paper" && choiceC =="rock")||
+    (choiceP ==="scissors" && choiceC =="paper")
+   ) {
+    return "Player";
+   } else{
+    return "Computer";
+}
+}
+function logWins() {
+    let playerWins = winners.filter((item)=> item == "Player").length;
+    let computerWins = winners.filter((item)=> item == "Computer").length;
+    let tie = winners.filter((item) => item == "Tie").length;
+    console.log("Results:");
+    console.log("Player wins:",playerWins);
+    console.log("Computer wins:",computerWins);
+    console.log("Tie:",tie);
+}
+function logRound(playerChoice,computerChoice,winner,round){
+    console.log("Round:", round);
+    console.log("Player Chose:",playerChoice);
+    console.log("Computer Choice:",computerChoice);
+    console.log(winner,"Won the Round");
+    console.log("------------------");
 
+}
 game();
